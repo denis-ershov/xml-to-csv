1f5db3266098402963fe96cd75af6619aac2be40
Denis Ershov <W1ns-Smile@yandex.ru>
2023-12-03 02:05:10 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,46 @@
-const tbodyCat = document.querySelector("tbody.cat-data");
+const tbodyRub = document.querySelector("tbody.rub-data");
 const tbodyTag = document.querySelector("tbody.tag-data");
+const checkCat = document.querySelector("#check-category");
+
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  document.querySelector("html").setAttribute("data-bs-theme", "dark");
+  document.querySelector(".dn").checked = true;
+}
+
+document.querySelector(".dn").addEventListener("click", () => {
+  document.querySelector(".dn").toggleAttribute("checked");
+  document.querySelector("html").toggleAttribute("data-bs-theme", "dark");
+  if (document.querySelector("html").getAttribute("data-bs-theme", "dark")) {
+    document.querySelector("html").removeAttribute("data-bs-theme");
+  } else {
+    document.querySelector("html").setAttribute("data-bs-theme", "dark");
+  }
+});
+
+if (!checkCat.checked) {
+  document.querySelector("#category").disabled = true;
+}
+
+checkCat.addEventListener("click", () => {
+  document.querySelector("#category").toggleAttribute("disabled");
+});
 
 function data() {
   const fileCat = document.querySelector("#category").files;
+  const fileRub = document.querySelector("#rubric").files;
   const fileTag = document.querySelector("#tag").files;
   const countFilesCat = fileCat.length;
+  const countFilesRub = fileRub.length;
   const countFilesTag = fileTag.length;
 
-  if (!countFilesCat) {
+  if (!countFilesCat && checkCat.checked) {
+    alert("Не выбран файл категорий!");
+    return;
+  }
+  if (!countFilesRub) {
     alert("Не выбран файл рубрик!");
     return;
   }
@@ -20,19 +53,53 @@ function data() {
 
   let tr = "";
 
-  const selectedFileCat = fileCat[0];
-  const readerCat = new FileReader();
-  readerCat.readAsText(selectedFileCat);
-  readerCat.addEventListener("load", (e) => {
-    let x2js = new X2JS();
-    let xmlText = readerCat.result;
-    let jsonObj = x2js.xml_str2json(xmlText);
-    let data = x2js.asArray(jsonObj.data.post);
-    let json = JSON.stringify(data);
-    for (let term in JSON.parse(json)) {
-      tr = document.createElement("tr");
-      tbodyCat.append(tr);
-      tr.innerHTML = "<td scope='col' widtd='5%'><input class='form-check-input' type='checkbox'></td><td scope='col' widtd='20%'>"+data[term].TermName+"</td><td scope='col' widtd='20%'>"+data[term].locative+"</td><td scope='col' widtd='20%'>"+data[term].TermSlug+"</td>";
+  const selectedFileRub = fileRub[0];
+  const readerRub = new FileReader();
+  readerRub.readAsText(selectedFileRub);
+  readerRub.addEventListener("load", (e) => {
+    let x2jsRub = new X2JS();
+    let xmlTextRub = readerRub.result;
+    let jsonObjRub = x2jsRub.xml_str2json(xmlTextRub);
+    let dataRub = x2jsRub.asArray(jsonObjRub.data.post);
+    let jsonRub = JSON.stringify(dataRub);
+
+    if (countFilesCat && checkCat.checked) {
+      const selectedFileCat = fileCat[0];
+      const readerCat = new FileReader();
+      readerCat.readAsText(selectedFileCat);
+      readerCat.addEventListener("load", (e) => {
+        let x2jsCat = new X2JS();
+        let xmlTextCat = readerCat.result;
+        let jsonObjCat = x2jsCat.xml_str2json(xmlTextCat);
+        let dataCat = x2jsCat.asArray(jsonObjCat.data.post);
+        let jsonCat = JSON.stringify(dataCat);
+
+        for (let termRub in JSON.parse(jsonRub)) {
+          if (jsonCat.includes(dataRub[termRub].TermName)) {
+            tr = document.createElement("tr");
+            tbodyRub.append(tr);
+            tr.innerHTML =
+              "<td scope='col' widtd='5%'><input class='form-check-input' type='checkbox' checked></td><td scope='col' widtd='20%'>" +
+              dataRub[termRub].TermName +
+              "</td><td scope='col' widtd='20%'>" +
+              dataRub[termRub].locative +
+              "</td><td scope='col' widtd='20%'>" +
+              dataRub[termRub].TermSlug +
+              "</td>";
+          } else {
+            tr = document.createElement("tr");
+            tbodyRub.append(tr);
+            tr.innerHTML =
+              "<td scope='col' widtd='5%'><input class='form-check-input' type='checkbox'></td><td scope='col' widtd='20%'>" +
+              dataRub[termRub].TermName +
+              "</td><td scope='col' widtd='20%'>" +
+              dataRub[termRub].locative +
+              "</td><td scope='col' widtd='20%'>" +
+              dataRub[termRub].TermSlug +
+              "</td>";
+          }
+        }
+      });
     }
   });
 
@@ -40,49 +107,96 @@ function data() {
   const readerTag = new FileReader();
   readerTag.readAsText(selectedFileTag);
   readerTag.addEventListener("load", (e) => {
-    let x2js = new X2JS();
-    let xmlText = readerTag.result;
-    let jsonObj = x2js.xml_str2json(xmlText);
-    let data = x2js.asArray(jsonObj.data.post);
-    for (let term in data) {
-      tr = document.createElement("tr");
-      tbodyTag.append(tr);
-      tr.innerHTML = "<td scope='col' widtd='5%'><input class='form-check-input' type='checkbox'></td><td scope='col' widtd='20%'>"+data[term].TermName+"</td><td scope='col' widtd='20%'>"+data[term].locative+"</td><td scope='col' widtd='20%'>"+data[term].TermSlug+"</td>";
+    let x2jsTag = new X2JS();
+    let xmlTextTag = readerTag.result;
+    let jsonObjTag = x2jsTag.xml_str2json(xmlTextTag);
+    let dataTag = x2jsTag.asArray(jsonObjTag.data.post);
+    let jsonTag = JSON.stringify(dataTag);
+
+    if (countFilesCat && checkCat.checked) {
+      const selectedFileCat = fileCat[0];
+      const readerCat = new FileReader();
+      readerCat.readAsText(selectedFileCat);
+      readerCat.addEventListener("load", (e) => {
+        let x2jsCat = new X2JS();
+        let xmlTextCat = readerCat.result;
+        let jsonObjCat = x2jsCat.xml_str2json(xmlTextCat);
+        let dataCat = x2jsCat.asArray(jsonObjCat.data.post);
+        let jsonCat = JSON.stringify(dataCat);
+
+        for (let termTag in JSON.parse(jsonTag)) {
+          if (jsonCat.includes(dataTag[termTag].TermName)) {
+            tr = document.createElement("tr");
+            tbodyTag.append(tr);
+            tr.innerHTML =
+              "<td scope='col' widtd='5%'><input class='form-check-input' type='checkbox' checked></td><td scope='col' widtd='20%'>" +
+              dataTag[termTag].TermName +
+              "</td><td scope='col' widtd='20%'>" +
+              dataTag[termTag].locative +
+              "</td><td scope='col' widtd='20%'>" +
+              dataTag[termTag].TermSlug +
+              "</td>";
+          } else {
+            tr = document.createElement("tr");
+            tbodyTag.append(tr);
+            tr.innerHTML =
+              "<td scope='col' widtd='5%'><input class='form-check-input' type='checkbox'></td><td scope='col' widtd='20%'>" +
+              dataTag[termTag].TermName +
+              "</td><td scope='col' widtd='20%'>" +
+              dataTag[termTag].locative +
+              "</td><td scope='col' widtd='20%'>" +
+              dataTag[termTag].TermSlug +
+              "</td>";
+          }
+        }
+      });
     }
   });
 }
 
 function generate() {
-  let tableCatInfo = Array.prototype.map.call(document.querySelectorAll('.cat-data tr'), function(tr){
-    if (tr.querySelector("td:nth-child(1) > input").checked) {
-    return Array.prototype.map.call(tr.querySelectorAll('td'), function(td){
-      return td.innerHTML;
-      });
-    }
-    });
-
-    let tableTagInfo  = Array.prototype.map.call(document.querySelectorAll('.tag-data tr'), function(tr){
+  let tableRubInfo = Array.prototype.map.call(
+    document.querySelectorAll(".rub-data tr"),
+    function (tr) {
       if (tr.querySelector("td:nth-child(1) > input").checked) {
-      return Array.prototype.map.call(tr.querySelectorAll('td'), function(td){
-          return td.innerHTML;
-        });
+        return Array.prototype.map.call(
+          tr.querySelectorAll("td"),
+          function (td) {
+            return td.innerHTML;
+          }
+        );
       }
-      });
-  
-    const name_category = [];
-    const name_category_slug = [];
-    for (let tmc in tableCatInfo) {
-      if (tableCatInfo[tmc] !== undefined) {
-      name_category.push(String(tableCatInfo[tmc][1]));
-      name_category_slug.push(tableCatInfo[tmc][3]);
     }
-    }
+  );
 
-    const name_tag = [];
-    const name_org = [];
-    const name_tag_slug = [];
-    for (let tmt in tableTagInfo) {
-      if (tableTagInfo[tmt] !== undefined) {
+  let tableTagInfo = Array.prototype.map.call(
+    document.querySelectorAll(".tag-data tr"),
+    function (tr) {
+      if (tr.querySelector("td:nth-child(1) > input").checked) {
+        return Array.prototype.map.call(
+          tr.querySelectorAll("td"),
+          function (td) {
+            return td.innerHTML;
+          }
+        );
+      }
+    }
+  );
+
+  const name_category = [];
+  const name_category_slug = [];
+  for (let tmc in tableRubInfo) {
+    if (tableRubInfo[tmc] !== undefined) {
+      name_category.push(String(tableRubInfo[tmc][1]));
+      name_category_slug.push(tableRubInfo[tmc][3]);
+    }
+  }
+
+  const name_tag = [];
+  const name_org = [];
+  const name_tag_slug = [];
+  for (let tmt in tableTagInfo) {
+    if (tableTagInfo[tmt] !== undefined) {
       if (tableTagInfo[tmt][2] !== "") {
         name_tag.push(tableTagInfo[tmt][2]);
       } else {
@@ -91,7 +205,7 @@ function generate() {
       name_tag_slug.push(tableTagInfo[tmt][3]);
       name_org.push(tableTagInfo[tmt][1]);
     }
-    }
+  }
 
   setTimeout(() => {
     let crossName = d3.cross(name_category, name_tag);
